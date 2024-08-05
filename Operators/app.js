9dcd6405e51ce24755598fe53990c7b96ee02ce1
Muhammad Usman Khan <160340063+pixelman10@users.noreply.github.com>
2024-08-05 12:50:37 +0500

    Add files via upload

---
+++ Operators/app.js
@@ -0,0 +1,63 @@
+"use strict";
+//CLASS 6
+// npm init -y(package.json file)
+//node package(npm i @types/node -D)
+// let a: number = 5;
+// let b: number = 2;
+// let c: number;
+// c = ++a + a++ + --b + b-- + a + b++ + b--;
+//   6  +  6  +  1   + 1  + 7 + 0    +  1 : the answer will become 22.
+// console.log("2" + c);
+// Logical OPERATORS:
+(5 > 0) && (2 > 0); //IS TRUE    &&:AND AND OPERATOR  
+(5 < 0) || (5 > 0); //IS TRUE     ||:OR OPERATOR 
+!(5 > 0); //IS FALSE              !:Not operator
+let a = 5;
+let b = 7;
+let c = 9;
+let d = 10;
+let andOperator = (a > 0) && (b > 0) && (c > 0); //answer is true.           AIK bhi value yah condition false ho yeh answer false de dega .It is used tod first false value.
+console.log(andOperator);
+let andOperator2 = (a > 6) && (b > 0) && (c > 0); //answer is false.           AIK bhi value yah condition false ho yeh answer false de dega .It is used tod first false value.
+console.log(andOperator2);
+let orOperator = (a > 6) || (b > 6) || (c > 6); //answer is true.              AIK bhi value yah condition true ho yeh answer true de dega .It is used tod first true value. 
+console.log(orOperator);
+let notOperator = !(a < 0); //answer is true                               It is used to inverse the answer.True becomes false and false becomes true
+console.log(notOperator);
+let cities = ["Karachi", "Paris", "Jaddah", "New jersey"];
+cities.forEach(city => {
+    console.log(`${city}This is my one of the luxury country.`);
+});
+cities.map((items) => console.log(`${items} is one of the most rated city.`));
+//IF and IF-ELSE STATEMENTS:
+let isRaining = true;
+if (isRaining) {
+    console.log("Wear a raincoat.");
+}
+else {
+    console.log("Wear a sunglasses.");
+}
+//OR
+if (isRaining == true) {
+    console.log("Wear a raincoat.");
+}
+else {
+    console.log("Wear a sunglasses.");
+}
+let isRaining2 = false;
+if (isRaining2) {
+    console.log("Wear a raincoat.");
+}
+else {
+    console.log("Wear a sunglasses.");
+}
+let weather = "clody";
+if (isRaining) {
+    console.log("Baarish ho rhi hai.");
+}
+else if (weather == "cloudy") {
+    console.log("Barish ho skti hai");
+}
+else {
+    console.log("Barish nhi ho rhi.");
+}
---
+++ Operators/main.ts
@@ -0,0 +1,78 @@
+//CLASS 5
+
+//Unary Operators:
+let a=5;
+let b=7;
+let cart1=a++;//answer is 5.
+let cart2=--b;//answer is 6.
+
+//Prefix icrement:
+let cart3=++b;//answer is 7.
+let cart4=--a;//answer is 5.
+
+//Postfix Increment:
+let cart5=b++;//answer is 7.    //postfix mai value update next line pe hoti hai.uss waqt uski value wohi rahegee .Lekin agli line mai change hojaye gee .Just like that:
+let cart7=b;//answer is 8.
+let cart6=a--;//answer is 5.
+
+console.log(cart1);
+console.log(cart2);
+console.log(cart3);
+console.log(cart4);
+console.log(cart5);
+console.log(cart6);
+console.log(cart7);//answer is 8.
+console.log("line 25",b++);//answer is 8 because last line mai answer 8 tha.Ab next line mai yeh 9 hojaye ga.
+console.log("Line 26",b);//answer is 9.
+
+
+//Combining Operators:
+console.log(4+4*5);//answer is 24 because BODMAS Rule is applied.The answer is not 40. 
+console.log(5 + 2 - 3 * 2 / 2 % 2);//its answer is 6 because BODMAS rule is applied.
+
+/*BODMAS stands for:
+
+Brackets
+Orders (i.e., powers and roots, such as squares and square roots)
+Division
+Multiplication
+Addition
+Subtraction*/
+
+let c=4;
+let d=2;
+console.log(++c + c++ + ++d + d++);//the answer is 16.
+console.log(--c - c++ + --d - d++);//This answer is 0.
+
+//ASSIGNMENT OPERATOR:(=)
+let e=5;
+e +=6;
+// e -=6;
+// e *=6;
+// e /=6;
+console.log(e);//the answer is 11 because e contain the value of 5.
+
+//Comparison Operator:
+//5==2 is false  
+//5!=2 is true    
+//5>2 is true
+//5<2 is false
+
+let num1: number = 12;
+let num2: number = 24;
+
+let equalComaparison = num1 == num2;//answer is false.
+let isNotequalComaparison = num1 != num2;//answer is true.      != means not operator.
+let greaterthanComaparison = num1 > num2;//answer is false.
+let lessthanComaparison = num1 < num2;//answer is true.
+
+console.log(equalComaparison);
+console.log(isNotequalComaparison);
+console.log(greaterthanComaparison);
+console.log(lessthanComaparison); 
+
+
+
+
+
+
